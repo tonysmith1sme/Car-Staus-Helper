9017c001cf620eb16931307a583fcc6af7e89b24
liyiwei <15897461476@139.com>
2022-11-28 22:48:31 +0800

    添加遗漏文件

---
+++ app/src/main/java/android/hardware/bydauto/radar/BYDAutoRadarDevice.java
@@ -0,0 +1,18 @@
+package android.hardware.bydauto.radar;
+
+import android.content.Context;
+
+public class BYDAutoRadarDevice {
+    //public class BYDAutoRadarDevice extends AbsBYDAutoDevice {
+    public static BYDAutoRadarDevice getInstance(Context con) {
+        throw new RuntimeException("Stub!");
+    }
+
+    public void registerListener(AbsBYDAutoRadarListener l) {
+        throw new RuntimeException("Stub!");
+    }
+
+    public void unregisterListener(AbsBYDAutoRadarListener l) {
+        throw new RuntimeException("Stub!");
+    }
+}
